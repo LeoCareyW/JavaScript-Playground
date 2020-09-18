156f9eb40be1482a295a7ea3846bf4891c01ca9f
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-09-18 15:44:20 +0100

    event listeners added to buttons

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -2,15 +2,34 @@
 
 // const p = document.querySelector('p')
 // p.remove()
+const notes = [{
+  title: 'My next trip',
+  body: 'I would like to go to Cuba'
+}, {
+  title: 'Habbits to work on',
+  body: 'Exercise'
+}, {
+  title: 'Office modifications',
+  body: 'New monitor'
+}]
 
-const ps = document.querySelectorAll('p')
-
-ps.forEach(function (p) {
-  p.textContent = '********'
-  // console.log(p.textContent)
-  // p.remove()
+document.querySelector('#create-note').addEventListener('click', function (e) {
+  e.target.textContent = 'The button was clicked'
 })
 
-const newPara = document.createElement('p')
-newPara.textContent = 'This is a new element from JavaScript'
-document.querySelector('body').appendChild(newPara)
+document.querySelector('#remove-note-button').addEventListener('click', function () {
+  document.querySelectorAll('.note').forEach(function(note) {
+    note.remove()
+  })
+})
+// const ps = document.querySelectorAll('p')
+
+// ps.forEach(function (p) {
+//   p.textContent = '********'
+//   // console.log(p.textContent)
+//   // p.remove()
+// })
+
+// const newPara = document.createElement('p')
+// newPara.textContent = 'This is a new element from JavaScript'
+// document.querySelector('body').appendChild(newPara)
--- todo-app/todo-html.js
+++ todo-app/todo-html.js
@@ -16,7 +16,34 @@ const todos = [
   completed: true
 }]
 
+// let uncompleted = 0
+// todos.forEach((item) => {
+//   if (!item.completed)
+//     uncompleted += 1
+//   })
 
+tasks = []
+  todos.forEach((item) => {
+    tasks.push(item.title)
+  })
+
+  const incompleteTodos = todos.filter((todo) => {
+    return !todo.completed
+  })
+
+  const newPara = document.createElement('h2')
+    newPara.textContent = `You have ${incompleteTodos.length} tasks left to do.`
+    document.querySelector('body').appendChild(newPara)
+
+  tasks.forEach((task, index) => {
+    const newPara =  document.createElement('p')
+    newPara.textContent = `${index + 1}) ${task}`
+    document.querySelector('body').appendChild(newPara)
+  })
+
+document.querySelector('#add-todo-button').addEventListener('click', function (e) {
+  e.target.textContent = 'Button clicked'
+})
 
 
 
@@ -26,3 +53,20 @@ const todos = [
 //   if (p.textContent.includes('the'))
 //     p.remove()
 // });
+
+// DOM - Document Object Model
+
+// const p = document.querySelector('p')
+// p.remove()
+
+// const ps = document.querySelectorAll('p')
+
+// ps.forEach(function (p) {
+//   p.textContent = '********'
+//   // console.log(p.textContent)
+//   // p.remove()
+// })
+
+// const newPara = document.createElement('p')
+// newPara.textContent = 'This is a new element from JavaScript'
+// document.querySelector('body').appendChild(newPara)
